ef2a6d916367897fa77baefb14d2dc3c6b675004
Narek Hovhannisyan <narekgilmour@gmail.com>
2023-11-14 09:48:47 +0400

    config: lint jest.

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  transformIgnorePatterns: [`/node_modules/.*`],
+  transformIgnorePatterns: ['/node_modules/.*'],
   transform: {
     '^.+\\.ts?$': [
       'ts-jest',
