19d75cab3498281f7a46c6fa869e9eb72fc48bd0
Narek Hovhannisyan <narekgilmour@gmail.com>
2023-11-21 17:16:09 +0400

    lib: implement error layer for cloud instance  metadata.

--- src/lib/cloud-instance-metadata/index.ts
+++ src/lib/cloud-instance-metadata/index.ts
@@ -1,13 +1,20 @@
 import {ModelPluginInterface} from '../../interfaces';
 
-import {KeyValuePair} from '../../types/common';
+import {ERRORS} from '../../util/errors';
+
 import * as AWS_INSTANCES from './aws-instances.json';
 import * as AZURE_INSTANCES from './azure-instances.json';
+
+import {KeyValuePair, ModelParams} from '../../types/common';
+import {buildErrorMessage} from '../../util/helpers';
+
+const {InputValidationError} = ERRORS;
 
 export class CloudInstanceMetadataModel implements ModelPluginInterface {
   authParams: object | undefined = undefined;
   staticParams: object | undefined;
   name: string | undefined;
+  errorBuilder = buildErrorMessage(CloudInstanceMetadataModel);
 
   authenticate(authParams: object): void {
     this.authParams = authParams;
@@ -18,47 +25,67 @@ export class CloudInstanceMetadataModel implements ModelPluginInterface {
    * @param {Object[]} inputs
    * @param {string} inputs[].timestamp RFC3339 timestamp string
    */
-  async execute(inputs: object | object[] | undefined): Promise<any[]> {
+  async execute(inputs: ModelParams[]): Promise<any[]> {
     if (inputs === undefined) {
-      throw new Error('Required Parameters not provided');
-    } else if (!Array.isArray(inputs)) {
-      throw new Error('inputs must be an array');
+      throw new InputValidationError(
+        this.errorBuilder({message: 'Input data is missing'})
+      );
+    }
+
+    if (!Array.isArray(inputs)) {
+      throw new InputValidationError(
+        this.errorBuilder({message: 'Input data is not an array'})
+      );
     }
 
     return inputs.map((input: KeyValuePair) => {
       let vendor = '';
       let instance_type = '';
-      if ('cloud-vendor' in input) {
-        vendor = input['cloud-vendor'];
-      } else {
-        throw new Error('Each input must contain a cloud-vendor key');
+
+      if (!('cloud-vendor' in input)) {
+        throw new InputValidationError(
+          this.errorBuilder({
+            message: "Each input must contain a 'cloud-vendor' key",
+          })
+        );
       }
-      // if ('cloud-region' in input) {
-      //   region = input['cloud-region'];
-      // } else {
-      //   throw new Error('Each input must contain a cloud-region key');
-      // }
+
+      vendor = input['cloud-vendor'];
+
       if ('cloud-instance-type' in input) {
         instance_type = input['cloud-instance-type'];
       } else {
-        throw new Error('Each input must contain a cloud-instance-type key');
-      }
-      // console.log(['aws', 'azure'].includes(vendor));
-      if (!['aws', 'azure'].includes(vendor)) {
-        throw new Error(
-          'cloud-vendor: Only `aws`/`azure` is currently supported'
+        throw new InputValidationError(
+          this.errorBuilder({
+            message: "Each input must contain a 'cloud-instance-type' key",
+          })
         );
       }
+
+      const clouds = ['aws', 'azure'];
+
+      if (!clouds.includes(vendor)) {
+        throw new InputValidationError(
+          this.errorBuilder({
+            scope: 'cloud-vendor',
+            message: "Only 'aws'/'azure' is currently supported",
+          })
+        );
+      }
+
       if (vendor === 'aws') {
         const instance = AWS_INSTANCES.find(
           instance => instance['Instance type'] === instance_type
         );
+
         if (instance) {
           input['vcpus-allocated'] = instance['Instance vCPU'];
           input['vcpus-total'] = instance['Platform Total Number of vCPU'];
           input['memory-available'] = instance['Instance Memory (in GB)'];
           const cpuType = instance['Platform CPU Name'];
+
           let platform = '';
+
           if (cpuType.startsWith('EPYC')) {
             platform = 'AMD';
           } else if (cpuType.startsWith('Xeon')) {
@@ -70,14 +97,18 @@ export class CloudInstanceMetadataModel implements ModelPluginInterface {
           }
           input['physical-processor'] = `${platform} ${cpuType}`;
         } else {
-          throw new Error(
-            `cloud-instance-type: ${instance_type} is not supported in vendor: ${vendor}`
+          throw new InputValidationError(
+            this.errorBuilder({
+              scope: 'cloud-instance-type',
+              message: `'${instance_type}' is not supported in '${vendor}'`,
+            })
           );
         }
       } else if (vendor === 'azure') {
         const instance = AZURE_INSTANCES.find(
           instance => instance['instance-type'] === instance_type
         );
+
         if (instance) {
           input['vcpus-allocated'] = instance['cpu-cores-utilized'];
           input['vcpus-total'] = instance['cpu-cores-available'];
@@ -85,11 +116,15 @@ export class CloudInstanceMetadataModel implements ModelPluginInterface {
           input['memory-available'] = instance['memory-available'];
           input['thermal-design-power'] = instance['thermal-design-power'];
         } else {
-          throw new Error(
-            `cloud-instance-type: ${instance_type} is not supported in vendor: ${vendor}`
+          throw new InputValidationError(
+            this.errorBuilder({
+              scope: 'cloud-instance-type',
+              message: `'${instance_type}' is not supported in '${vendor}'`,
+            })
           );
         }
       }
+
       return input;
     });
   }
