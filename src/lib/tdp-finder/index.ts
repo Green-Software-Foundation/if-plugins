1c6e067f29eb4bc084ca36d68bab2a449513d205
Narek Hovhannisyan <narekgilmour@gmail.com>
2023-11-21 19:44:45 +0400

    lib: implement error layer for tdp finder.

--- src/lib/tdp-finder/index.ts
+++ src/lib/tdp-finder/index.ts
@@ -1,13 +1,20 @@
-import {ModelPluginInterface} from '../../interfaces';
-import {KeyValuePair} from '../../types/common';
 import * as fs from 'fs';
 import * as path from 'path';
+
+import {ERRORS} from '../../util/errors';
+import {buildErrorMessage} from '../../util/helpers';
+
+import {ModelPluginInterface} from '../../interfaces';
+import {KeyValuePair} from '../../types/common';
+
+const {InputValidationError, UnsupportedValueError} = ERRORS;
 
 export class TdpFinderModel implements ModelPluginInterface {
   authParams: object | undefined = undefined;
   staticParams: object | undefined;
   name: string | undefined;
   data: any;
+  errorBuilder = buildErrorMessage(TdpFinderModel.name);
 
   authenticate(authParams: object): void {
     this.authParams = authParams;
@@ -22,12 +29,18 @@ export class TdpFinderModel implements ModelPluginInterface {
    */
   async execute(inputs: object | object[] | undefined): Promise<any[]> {
     if (inputs === undefined) {
-      throw new Error('Required Parameters not provided');
-    } else if (!Array.isArray(inputs)) {
-      throw new Error('Inputs must be an array');
+      throw new InputValidationError(
+        this.errorBuilder({message: 'Input data is missing'})
+      );
     }
 
-    return inputs.map((input: KeyValuePair) => {
+    if (!Array.isArray(inputs)) {
+      throw new InputValidationError(
+        this.errorBuilder({message: 'Input data is not an array'})
+      );
+    }
+
+    return inputs.map((input: KeyValuePair, index: number) => {
       input['thermal-design-power'] = 0;
       if ('physical-processor' in input) {
         const physicalProcessors = input['physical-processor'] as string;
@@ -39,13 +52,19 @@ export class TdpFinderModel implements ModelPluginInterface {
           ) {
             input['thermal-design-power'] = this.data[physicalProcessor];
           } else if (!(physicalProcessor in this.data)) {
-            throw new Error(
-              `physical-processor ${physicalProcessor} not found in database. Please check spelling / contribute to IEF with the data.`
+            throw new UnsupportedValueError(
+              this.errorBuilder({
+                message: `'physical-processor': ${physicalProcessor} from input[${index}] is not found in database. Please check spelling / contribute to 'if-models' with the data.`,
+              })
             );
           }
         });
       } else {
-        throw new Error('physical-processor not provided');
+        throw new InputValidationError(
+          this.errorBuilder({
+            message: `'physical-processor' not provided in input[${index}].`,
+          })
+        );
       }
       return input;
     });
@@ -56,6 +75,7 @@ export class TdpFinderModel implements ModelPluginInterface {
   ): Promise<ModelPluginInterface> {
     this.staticParams = staticParams;
     this.data = await this.loadData();
+
     return this;
   }
 
@@ -69,29 +89,37 @@ export class TdpFinderModel implements ModelPluginInterface {
       const tdp = parseFloat(tdp_r.replace('\r', ''));
       data[name] = tdp;
     }
+
     const result2 = fs.readFileSync(path.join(__dirname, 'data2.csv'), 'utf8');
     for (const line of result2.split('\n')) {
       const [name_w_at, tdp_r] = line.split(',');
+
       if (name_w_at === '') {
         continue;
       }
+
       const name = name_w_at.split('@')[0].trim();
       const tdp = parseFloat(tdp_r.replace('\r', ''));
+
       if (!(name in data) || data[name] < tdp) {
         data[name] = tdp;
       }
     }
+
     const result3 = fs.readFileSync(
       path.join(__dirname, 'boavizta_data.csv'),
       'utf8'
     );
     for (const line of result3.split('\n')) {
       const [name_w_at, tdp_r] = line.split(',');
+
       if (name_w_at === '') {
         continue;
       }
+
       const name = name_w_at.split('@')[0].trim();
       const tdp = parseFloat(tdp_r.replace('\r', ''));
+
       if (!(name in data) || data[name] < tdp) {
         data[name] = tdp;
       }
