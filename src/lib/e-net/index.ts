95ac91ef91b28432a6824ff4e8be0e772871c729
Paz Barda <paz.barda@intel.com>
2024-01-02 14:34:08 +0200

    Added e-net, a network model (based on e-shoppen model (case 'e-net')).

---
+++ src/lib/e-net/index.ts
@@ -0,0 +1,91 @@
+import {ModelPluginInterface} from '../../interfaces';
+
+import {ERRORS} from '../../util/errors';
+import {buildErrorMessage} from '../../util/helpers';
+
+import {ModelParams} from '../../types/common';
+
+const {InputValidationError} = ERRORS;
+
+export class ENetModel implements ModelPluginInterface {
+  authParams: object | undefined; // Defined for compatibility. Not used in this.
+  staticParams: object | undefined;
+
+  errorBuilder = buildErrorMessage(ENetModel);
+
+  async configure(
+    staticParams: object | undefined = undefined
+  ): Promise<ModelPluginInterface> {
+    if (staticParams === undefined) {
+      staticParams = {};
+    }
+    this.staticParams = staticParams;
+    return this;
+  }
+
+  /**
+   * Calculate the total emissions for a list of inputs.
+   *
+   * Each input require:
+   * @param {Object[]} inputs
+   * @param {string} inputs[].timestamp RFC3339 timestamp string
+   * @param {number} inputs[].mem-util percentage mem usage
+   */
+  async execute(inputs: ModelParams[]): Promise<ModelParams[]> {
+    if (inputs.length === 0) {
+      throw new InputValidationError(
+        this.errorBuilder({
+          message: 'Input data is missing',
+        })
+      );
+    }
+    return inputs.map((input: ModelParams) => {
+      input['energy-network'] = this.calculateEnergy(input);
+      return input;
+    });
+  }
+
+  // TODO PB - add units and better explanations to required params
+  /**
+   * Calculates the energy consumption for a single input
+   * requires
+   *
+   * data-in: inbound data
+   * data-out: outbound data
+   * net-energy: network energy cofficient
+   *
+   * multiplies (data-in + data-out) by net-energy and converts to kWh (div by 1000)
+   */
+  private calculateEnergy(input: ModelParams) {
+    this.validateInput(input);
+    const data_in = input['data-in'];
+    const data_out = input['data-out'];
+    const net_energy = input['net-energy']
+    return ((data_in + data_out) * net_energy) / 1000;
+  }
+
+  private validateInput(input: ModelParams) {
+    this.validateFieldInInput(input, 'data-in')
+    this.validateFieldInInput(input, 'data-out')
+    this.validateFieldInInput(input, 'net-energy')
+
+    if (input['net-energy'] === 0) {
+      throw new InputValidationError(
+        this.errorBuilder({
+          message: "'net-energy' set to zero",
+        })
+      );
+    }
+  }
+
+  private validateFieldInInput(input: ModelParams, field: string) {
+    if (!(field in input) || input[field] === undefined) {
+      throw new InputValidationError(
+        this.errorBuilder({
+          message: field + ' is missing or invalid',
+        })
+      );
+    }
+  }
+
+}
