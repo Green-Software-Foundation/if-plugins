f06e11cb592e31ee5886d7ea153d5b5c42341701
Narek Hovhannisyan <narekgilmour@gmail.com>
2024-01-10 15:45:59 +0400

    util: init validations.

---
+++ src/util/validations.ts
@@ -0,0 +1,29 @@
+import {ZodIssue, ZodSchema} from 'zod';
+
+import {ERRORS} from './errors';
+
+const {InputValidationError} = ERRORS;
+
+const prettifyErrorMessage = (issues: string) => {
+  const issuesArray = JSON.parse(issues);
+
+  return issuesArray.map((issue: ZodIssue) => {
+    const {code, path, message} = issue;
+    const flattenPath = path.map(part =>
+      typeof part === 'number' ? `[${part}]` : part
+    );
+    const fullPath = flattenPath.join('.');
+
+    return `"${fullPath}" parameter is ${message.toLowerCase()}. Error code: ${code}.`;
+  });
+};
+
+export const validate = (schema: ZodSchema, object: any) => {
+  const validationResult = schema.safeParse(object);
+
+  if (!validationResult.success) {
+    throw new InputValidationError(
+      prettifyErrorMessage(validationResult.error.message)
+    );
+  }
+};
