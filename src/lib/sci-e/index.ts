11b5a6e87ea9da77b1888e4db5fb3f896ccd3dff
Narek Hovhannisyan <narekgilmour@gmail.com>
2024-01-10 18:02:44 +0400

    lib: remove error builder from SciEModel.

--- src/lib/sci-e/index.ts
+++ src/lib/sci-e/index.ts
@@ -2,14 +2,10 @@ import {z} from 'zod';
 
 import {ModelPluginInterface} from '../../interfaces';
 
-import {buildErrorMessage} from '../../util/helpers';
-
 import {ModelParams} from '../../types/common';
 import {validate} from '../../util/validations';
 
 export class SciEModel implements ModelPluginInterface {
-  errorBuilder = buildErrorMessage(SciEModel);
-
   /**
    * Configures the SCI-E Plugin.
    */
@@ -33,24 +29,24 @@ export class SciEModel implements ModelPluginInterface {
    */
   private validateSingleInput(input: ModelParams) {
     const schema = z.object({
-      'energy-cpu': z.number().min(0).default(0),
-      'energy-memory': z.number().min(0).default(0),
-      'energy-network': z.number().min(0).default(0),
+      'energy-cpu': z.number().gte(0).min(0),
+      'energy-memory': z.number().gte(0).min(0),
+      'energy-network': z.number().gte(0).min(0),
     });
 
-    validate(schema, input);
+    return validate(schema, input);
   }
 
   /**
    * Calculates the sum of the energy components.
    */
   private calculateEnergy(input: ModelParams) {
-    this.validateSingleInput(input);
+    const safeInput = this.validateSingleInput(input);
 
     const energyMetrics = ['energy-cpu', 'energy-memory', 'energy-network'];
 
     return energyMetrics.reduce((acc, metric) => {
-      acc += input[metric];
+      acc += safeInput[metric];
 
       return acc;
     }, 0);
