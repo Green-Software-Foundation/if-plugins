757cfa1208c1895f6f8005ac7c5069785a226cf5
Narek Hovhannisyan <narekgilmour@gmail.com>
2024-01-10 18:27:46 +0400

    util: tune validations strategy, add at least one defined.

--- src/util/validations.ts
+++ src/util/validations.ts
@@ -4,6 +4,16 @@ import {ERRORS} from './errors';
 
 const {InputValidationError} = ERRORS;
 
+/**
+ * At least one property defined handler.
+ */
+export const atLeastOneDefined = (
+  obj: Record<string | number | symbol, unknown>
+) => Object.values(obj).some(v => v !== undefined);
+
+/**
+ * Error message formatter for zod issues.
+ */
 const prettifyErrorMessage = (issues: string) => {
   const issuesArray = JSON.parse(issues);
 
@@ -14,10 +24,17 @@ const prettifyErrorMessage = (issues: string) => {
     );
     const fullPath = flattenPath.join('.');
 
+    if (!fullPath) {
+      return message;
+    }
+
     return `"${fullPath}" parameter is ${message.toLowerCase()}. Error code: ${code}.`;
   });
 };
 
+/**
+ * Validates given `object` with given `schema`.
+ */
 export const validate = (schema: ZodSchema, object: any) => {
   const validationResult = schema.safeParse(object);
 
