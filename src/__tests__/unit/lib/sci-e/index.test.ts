923c8d88d2abec3e9b6afe70912bbb91eeb9244e
Narek Hovhannisyan <narekgilmour@gmail.com>
2024-01-10 18:03:24 +0400

    tests: refactor SCE-E model tests.

--- src/__tests__/unit/lib/sci-e/index.test.ts
+++ src/__tests__/unit/lib/sci-e/index.test.ts
@@ -1,40 +1,81 @@
-import {describe, expect, jest, test} from '@jest/globals';
 import {SciEModel} from '../../../../lib';
 
-jest.setTimeout(30000);
+import {ERRORS} from '../../../../util/errors';
 
-describe('sci-e:configure test', () => {
-  test('initialize with params', async () => {
-    const outputModel = new SciEModel();
-    await outputModel.configure();
-    await outputModel.configure({});
-    await expect(
-      outputModel.execute([
-        {
-          duration: 3600,
-          'energy-cpu': 1,
-          'energy-network': 1,
-          'energy-memory': 1,
-          timestamp: '2021-01-01T00:00:00Z',
-        },
-      ])
-    ).resolves.toStrictEqual([
-      {
-        duration: 3600,
-        'energy-cpu': 1,
-        'energy-network': 1,
-        'energy-memory': 1,
-        energy: 3,
-        timestamp: '2021-01-01T00:00:00Z',
-      },
-    ]);
-    await expect(
-      outputModel.execute([
-        {
-          duration: 3600,
-          timestamp: '2021-01-01T00:00:00Z',
-        },
-      ])
-    ).rejects.toThrow();
+const {InputValidationError} = ERRORS;
+
+describe('lib/sci-e: ', () => {
+  describe('SciEModel: ', () => {
+    describe('init: ', () => {
+      it('successfully initalized.', () => {
+        const outputModel = new SciEModel();
+
+        expect(outputModel).toHaveProperty('configure');
+        expect(outputModel).toHaveProperty('execute');
+      });
+    });
+
+    describe('configure(): ', () => {
+      it('should', async () => {
+        const outputModel = new SciEModel();
+        await outputModel.configure();
+
+        expect.assertions(1);
+
+        expect(outputModel).toBeInstanceOf(SciEModel);
+      });
+    });
+
+    describe('execute(): ', () => {
+      it('successfully runs with given input.', async () => {
+        const outputModel = new SciEModel();
+
+        expect.assertions(1);
+
+        const expectedResult = [
+          {
+            duration: 3600,
+            'energy-cpu': 1,
+            'energy-network': 1,
+            'energy-memory': 1,
+            energy: 3,
+            timestamp: '2021-01-01T00:00:00Z',
+          },
+        ];
+
+        const result = await outputModel.execute([
+          {
+            duration: 3600,
+            'energy-cpu': 1,
+            'energy-network': 1,
+            'energy-memory': 1,
+            timestamp: '2021-01-01T00:00:00Z',
+          },
+        ]);
+
+        expect(result).toStrictEqual(expectedResult);
+      });
+
+      it('should throw error in case if some params are missing from input.', async () => {
+        const outputModel = new SciEModel();
+        const expectedMessage =
+          '"energy-cpu" parameter is required. Error code: invalid_type.,"energy-memory" parameter is required. Error code: invalid_type.,"energy-network" parameter is required. Error code: invalid_type.';
+
+        expect.assertions(1);
+
+        try {
+          await outputModel.execute([
+            {
+              duration: 3600,
+              timestamp: '2021-01-01T00:00:00Z',
+            },
+          ]);
+        } catch (error) {
+          expect(error).toStrictEqual(
+            new InputValidationError(expectedMessage)
+          );
+        }
+      });
+    });
   });
 });
