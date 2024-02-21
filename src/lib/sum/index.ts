c4602386184ea2dd077fc8480b5afb7f814e0cdd
jmc <33655003+jmcook1186@users.noreply.github.com>
2024-02-21 12:58:58 +0000

    fix: (sum plugin): add validation for config values

--- src/lib/sum/index.ts
+++ src/lib/sum/index.ts
@@ -1,22 +1,47 @@
-import { PluginInterface } from '../../interfaces';
-import { PluginParams } from '../../types/common';
-import { buildErrorMessage } from '../../util/helpers';
-import { ERRORS } from '../../util/errors';
-const { InputValidationError } = ERRORS;
+import {PluginInterface} from '../../interfaces';
+import {PluginParams} from '../../types/common';
+import {buildErrorMessage} from '../../util/helpers';
+import {ERRORS} from '../../util/errors';
+import {SumConfig} from '../../types/sum';
 
-type sumConfig = {
-  inputParameters: string[];
-  outputParameter: string;
-};
+const {InputValidationError} = ERRORS;
 
-export const Sum = (globalConfig: sumConfig): PluginInterface => {
+export const Sum = (globalConfig: SumConfig): PluginInterface => {
   const errorBuilder = buildErrorMessage(Sum.name);
-  const inputParameters = globalConfig.inputParameters;
+  const inputParameters = globalConfig.inputParameters || [];
   const outputParameter = globalConfig.outputParameter;
   const metadata = {
     kind: 'execute',
   };
 
+  const validateInputs = (
+    inputParameters: string[],
+    outputParameter: string
+  ) => {
+    if (inputParameters.length === 0) {
+      throw new InputValidationError(
+        errorBuilder({
+          message: 'No input parameters were provided in global config.',
+        })
+      );
+    }
+    if (inputParameters.length === 1) {
+      throw new InputValidationError(
+        errorBuilder({
+          message:
+            'Only one input parameter was provided in global config. Cannot calculate sum of one number.',
+        })
+      );
+    }
+    if (!outputParameter || outputParameter === '') {
+      throw new InputValidationError(
+        errorBuilder({
+          message:
+            'The output parameter name was missing. Please provide one in global config',
+        })
+      );
+    }
+  };
   /**
    * Checks for required fields in input.
    */
@@ -37,6 +62,7 @@ export const Sum = (globalConfig: sumConfig): PluginInterface => {
    * Calculate the sum of each .
    */
   const execute = async (inputs: PluginParams[]): Promise<PluginParams[]> => {
+    validateInputs(inputParameters, outputParameter);
     inputs.map(input => {
       const safeInput = validateSingleInput(input);
       return calculateSum(safeInput, inputParameters, outputParameter);
@@ -52,11 +78,13 @@ export const Sum = (globalConfig: sumConfig): PluginInterface => {
     inputParameters: string[],
     outputParameter: string
   ) => {
-
-    input[outputParameter] = inputParameters.reduce((accumulator, metricToSum) => {
-      return accumulator + input[metricToSum];
-    }, 0);
-  }
+    input[outputParameter] = inputParameters.reduce(
+      (accumulator, metricToSum) => {
+        return accumulator + input[metricToSum];
+      },
+      0
+    );
+  };
 
   return {
     metadata,
