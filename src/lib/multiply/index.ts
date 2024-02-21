1e2296c6adc5efc75e51fb4000d44d0095364bfc
jmc <33655003+jmcook1186@users.noreply.github.com>
2024-02-21 11:50:41 +0000

    feat: (multiply plugin) export function from index

--- src/lib/multiply/index.ts
+++ src/lib/multiply/index.ts
@@ -1,66 +1,67 @@
-import { PluginInterface } from '../../interfaces';
-import { PluginParams } from '../../types/common';
-import { buildErrorMessage } from '../../util/helpers';
-import { ERRORS } from '../../util/errors';
-const { InputValidationError } = ERRORS;
+import {PluginInterface} from '../../interfaces';
+import {PluginParams} from '../../types/common';
+import {buildErrorMessage} from '../../util/helpers';
+import {ERRORS} from '../../util/errors';
+const {InputValidationError} = ERRORS;
 
 type MultiplyConfig = {
-    inputParameters: string[];
-    outputParameter: string;
+  inputParameters: string[];
+  outputParameter: string;
 };
 
-export const Sum = (globalConfig: MultiplyConfig): PluginInterface => {
-    const errorBuilder = buildErrorMessage(Sum.name);
-    const inputParameters = globalConfig.inputParameters;
-    const outputParameter = globalConfig.outputParameter;
-    const metadata = {
-        kind: 'execute',
-    };
+export const Multiply = (globalConfig: MultiplyConfig): PluginInterface => {
+  const errorBuilder = buildErrorMessage(Multiply.name);
+  const inputParameters = globalConfig.inputParameters;
+  const outputParameter = globalConfig.outputParameter;
+  const metadata = {
+    kind: 'execute',
+  };
 
-    /**
-     * Checks for required fields in input.
-     */
-    const validateSingleInput = (input: PluginParams) => {
-        inputParameters.forEach(metricToSum => {
-            if (!Object.getOwnPropertyDescriptor(input, metricToSum)) {
-                throw new InputValidationError(
-                    errorBuilder({
-                        message: `${metricToSum} is missing from the input array`,
-                    })
-                );
-            }
-        });
-        return input;
-    };
+  /**
+   * Checks for required fields in input.
+   */
+  const validateSingleInput = (input: PluginParams) => {
+    inputParameters.forEach(metricToMultiply => {
+      if (!Object.getOwnPropertyDescriptor(input, metricToMultiply)) {
+        throw new InputValidationError(
+          errorBuilder({
+            message: `${metricToMultiply} is missing from the input array`,
+          })
+        );
+      }
+    });
+    return input;
+  };
 
-    /**
-     * Calculate the sum of each .
-     */
-    const execute = async (inputs: PluginParams[]): Promise<PluginParams[]> => {
-        inputs.map(input => {
-            const safeInput = validateSingleInput(input);
-            return calculateProduct(safeInput, inputParameters, outputParameter);
-        });
-        return inputs;
-    };
+  /**
+   * Calculate the sum of each .
+   */
+  const execute = async (inputs: PluginParams[]): Promise<PluginParams[]> => {
+    inputs.map(input => {
+      const safeInput = validateSingleInput(input);
+      return calculateProduct(safeInput, inputParameters, outputParameter);
+    });
+    return inputs;
+  };
 
-    /**
-     * Calculates the sum of the energy components.
-     */
-    const calculateProduct = (
-        input: PluginParams,
-        inputParameters: string[],
-        outputParameter: string
-    ) => {
-        let sum = 0;
-        inputParameters.forEach(metricToSum => {
-            sum = sum * input[metricToSum];
-        });
-        return (input[outputParameter] = sum);
-    };
+  /**
+   * Calculates the sum of the energy components.
+   */
+  const calculateProduct = (
+    input: PluginParams,
+    inputParameters: string[],
+    outputParameter: string
+  ) => {
+    // in first iteration, 1 * metric == metric.
+    let product = 1;
+    inputParameters.forEach(metricToSum => {
+      product = product * input[metricToSum];
+    });
+    return (input[outputParameter] = product);
+  };
 
-    return {
-        metadata,
-        execute,
-    };
+  return {
+    metadata,
+    execute,
+  };
 };
